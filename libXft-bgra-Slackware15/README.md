691f40a6b124da872cabfadd1d1c2014fdf50a35
Isaac Yu <isaacyu1@isaacyu1.com>
2023-04-14 01:46:31 -0700

    Upload libXft-bgra build files for Slackware 15

---
+++ libXft-bgra-Slackware15/README.md
@@ -0,0 +1,23 @@
+## Introduction
+In Slackware 15.0, libXft does not provide BGRA glyph support by default.  
+BGRA support is required for displaying color emojis on suckless software (e.x. dwm).
+
+This script downloads all the files necessary for building libXft with the BGRA patch.  
+The patch was written by Maxime Coste:
+https://gitlab.freedesktop.org/xorg/lib/libxft/merge_requests/1.patch
+
+Unlike [OldTechBloke's](https://gitlab.com/OldTechBloke/slackware-libxft-bgra/) SlackBuild, this script sticks closely with the build files from the default Slackware repository.
+
+## Instructions
+Steps:
+1. Download my `libXft-bgra-Slackware15.sh` script and add executable permission:  
+```
+wget https://raw.githubusercontent.com/isaackwy/desktop-preferences/master/libXft-bgra-Slackware15/libXft-bgra-Slackware15.sh`
+chmod +x libXft-bgra-Slackware15.sh
+```
+
+2. Run the script. It downloads the files for building libXft. The script will also check the signature of the upstream libXft tarball:  
+`sh libXft-bgra-Slackware15.sh`
+
+3. Run the script:  
+`sudo ./x11.SlackBuild lib libXft`
---
+++ libXft-bgra-Slackware15/libXft-bgra-Slackware15.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# This script is for Slackware 15.0
+
+BUILD_DIR=libXft-bgra
+
+# Prepare folders
+mkdir -p $BUILD_DIR/configure
+mkdir -p $BUILD_DIR/patch/libXft
+mkdir -p $BUILD_DIR/slack-desc
+mkdir -p $BUILD_DIR/src/lib
+
+# Download files to the $BUILD_DIR directory
+wget https://mirrors.slackware.com/slackware/slackware64-15.0/source/x/x11/arch.use.flags -O $BUILD_DIR/arch.use.flags
+wget https://mirrors.slackware.com/slackware/slackware64-15.0/source/x/x11/modularize -O $BUILD_DIR/modularize
+wget https://mirrors.slackware.com/slackware/slackware64-15.0/source/x/x11/noarch -O $BUILD_DIR/noarch
+wget https://mirrors.slackware.com/slackware/slackware64-15.0/source/x/x11/package-blacklist -O $BUILD_DIR/package-blacklist
+wget https://mirrors.slackware.com/slackware/slackware64-15.0/source/x/x11/x11.SlackBuild -O $BUILD_DIR/x11.SlackBuild
+chmod +x $BUILD_DIR/x11.SlackBuild
+
+# Download files to subdirectories
+wget https://mirrors.slackware.com/slackware/slackware64-15.0/source/x/x11/configure/configure -O $BUILD_DIR/configure/configure
+wget https://gitlab.freedesktop.org/xorg/lib/libxft/-/merge_requests/1.patch -O $BUILD_DIR/patch/libXft/libXft-bgra-support.patch
+gzip $BUILD_DIR/patch/libXft/libXft-bgra-support.patch
+wget https://mirrors.slackware.com/slackware/slackware64-15.0/source/x/x11/slack-desc/libXft -O $BUILD_DIR/slack-desc/libXft
+echo 'zcat $CWD/patch/libXft/libXft-bgra-support.patch.gz | patch -p1 --verbose || { touch ${SLACK_X_BUILD_DIR}/${PKGNAME}.failed ; continue ; }' > $BUILD_DIR/patch/libXft.patch
+wget https://xorg.freedesktop.org/releases/individual/lib/libXft-2.3.4.tar.gz -O $BUILD_DIR/src/lib/libXft-2.3.4.tar.gz
+wget https://xorg.freedesktop.org/releases/individual/lib/libXft-2.3.4.tar.gz.sig -O $BUILD_DIR/src/lib/libXft-2.3.4.tar.gz.sig
+
+# Check tarball signature, then remove the libXft signature file
+gpg --keyserver keyserver.ubuntu.com --recv-keys 28C642A7
+gpg --verify $BUILD_DIR/src/lib/libXft-2.3.4.tar.gz.sig
+rm $BUILD_DIR/src/lib/libXft-2.3.4.tar.gz.sig
+
+# Final step: Build libXft (with the bgra patch):
+#sudo ./x11.SlackBuild lib libXft
