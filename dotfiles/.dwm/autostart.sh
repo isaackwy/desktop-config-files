30e60222a6eee6d680cf233a5a0bc1e8bbe1ac4d
Isaac Yu <isaacyu1@isaacyu1.com>
2023-04-15 01:22:03 -0700

    Edit ~/.dwm/autostart.sh

--- dotfiles/.dwm/autostart.sh
+++ dotfiles/.dwm/autostart.sh
@@ -2,17 +2,20 @@
 syncthing serve --no-browser --no-restart --logflags=0 &
 
 # Start DBUS session bus:
-if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-   eval $(dbus-launch --sh-syntax --exit-with-session)
-fi
+[ -z "$DBUS_SESSION_BUS_ADDRESS" ] && eval $(dbus-launch --sh-syntax --exit-with-session)
+
+# If starting dwm, load dwmblocks. If reloading dwm, restart dwmblocks.
+dwmblocks_load () {
+    [ ! $(pidof dwmblocks) ] && dwmblocks || (pkill dwmblocks && dwmblocks) 
+}
 
 # Start other programs.
 picom &
-conky &
+[ ! $(pidof conky) ] && conky &                     # Only load conky when starting dwm.
 pcmanfm-qt --desktop &
 dunst &
 pulseaudio --start; kill -44 $(pidof dwmblocks) &   # Display volume at startup as well. This assumes that the volume module has assigned the update signal to 10.
-dwmblocks &
+dwmblocks_load &
 nm-applet &
 fcitx &
 hp-systray -x &
